3423ec3e7a12043a5f20935dba9fd302f3ea6a7c
LamCao1130 <caohailam3011@gmail.com>
2025-10-15 02:08:12 +0700

    fix bug

---
+++ src/main/java/com/he181464/be_class/entity/ClassRoom.java
@@ -0,0 +1,35 @@
+package com.he181464.be_class.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "class_rooms")
+public class ClassRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "teacher_id", nullable = false)
+    private Long teacherId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private Account teacher;
+
+
+}
