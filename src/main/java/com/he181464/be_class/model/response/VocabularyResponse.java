e4691ef6bea914bef6110f5a0247785bd2c3151f
khaihuy1 <huytkhe180103@fpt.edu.vn>
2025-10-16 10:15:15 +0700

    Add lesson and vocabulary entity

---
+++ src/main/java/com/he181464/be_class/model/response/VocabularyResponse.java
@@ -0,0 +1,12 @@
+package com.he181464.be_class.model.response;
+
+public class VocabularyResponse {
+    private Long id;
+    private String word;
+    private String meaning;
+    private String pronunciation;
+    private String exampleSample;
+    private String imageUrl;
+    private String audioUrl;
+    private Integer sortOrder;
+}
---
+++ src/main/java/com/he181464/be_class/repository/LessonRepository.java
@@ -0,0 +1,7 @@
+package com.he181464.be_class.repository;
+
+import com.he181464.be_class.entity.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LessonRepository extends JpaRepository<Lesson,Long> {
+}
