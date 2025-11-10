0d126101740470f664d8a2b2cc743a4af33dad8c
LamCao1130 <caohailam3011@gmail.com>
2025-11-10 13:48:45 +0700

    add

--- src/main/java/com/he181464/be_class/repository/ReadingRepository.java
+++ src/main/java/com/he181464/be_class/repository/ReadingRepository.java
@@ -3,5 +3,8 @@ package com.he181464.be_class.repository;
 import com.he181464.be_class.entity.ReadingPassage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReadingRepository extends JpaRepository<ReadingPassage,Integer> {
+    List<ReadingPassage> findByLessonId(Long lessonId);
 }
